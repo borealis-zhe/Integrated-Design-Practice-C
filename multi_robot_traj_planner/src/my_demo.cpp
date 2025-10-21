7fc46aa2064375c5ef5aa82ed1802831f2502303
cz <2251087@tongji.edu.cn>
2025-10-21 20:23:51 +0800

    add demo for random start&destination

---
+++ multi_robot_traj_planner/src/my_demo.cpp
@@ -0,0 +1,39 @@
+#include <random>
+#include <fstream>
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+
+// 随机起点终点，生成mission_random.json，32个机器人
+
+int main() {
+    int N = 32;
+    double xy_min = -5.0, xy_max = 5.0, theta = 1.0;
+    double radius = 0.15, speed = 1.0;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(xy_min, xy_max);
+
+    json mission;
+    mission["robots"] = {
+        {"robot", {{"radius", radius}, {"speed", speed}}},
+        {"default", {{"radius", radius}, {"speed", speed}}},
+        {"seed", {{"seed", {rd()}}}}
+    };
+
+    for (int i = 0; i < N; ++i) {
+        json agent = {
+            {"name", "robot"},
+            {"start", {dis(gen), dis(gen), theta}},
+            {"goal", {dis(gen), dis(gen), theta}},
+            {"radius", radius},
+            {"speed", speed}
+        };
+        mission["agents"].push_back(agent);
+    }
+
+    std::ofstream file("mission_random.json");
+    file << mission.dump(2);
+    file.close();
+    return 0;
+}
